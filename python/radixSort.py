b403cdce64a904760e967e6ae5024ac61ae10c76
rowvln <7840167+rowvinn@users.noreply.github.com>
2022-06-11 22:41:43 -0700

    added time complexity and space complexity of radix sort

--- python/radixSort.py
+++ python/radixSort.py
@@ -5,27 +5,43 @@
 # More digits means a bigger number!
 # Sorts by putting numbers in different buckets starting from the rightmost number and moving to the leftmost side. 
 # After each sort, put them together then move index to to the right
-import math
 
-def getDigit(num, i):
-    return math.floor(abs(num) / pow(10, i) % 10)
+# Using counting sort to sort the elements in the basis of significant places
+def countingSort(array, place):
+    size = len(array)
+    output = [0] * size
+    count = [0] * 10
 
-def digitCount(num):
-    if num == 0: 
-        return 1
-    return math.floor(math.log10(abs(num))) + 1
+    # Calculate count of elements
+    for i in range(0, size):
+        index = array[i] // place
+        count[index % 10] += 1
 
-def mostDigits(nums):
-    maxDigits = 0
-    for i in range(len(nums)):
-        maxDigits = max(maxDigits, digitCount(nums[i]))
-    return maxDigits
+    # Calculate cumulative count
+    for i in range(1, 10):
+        count[i] += count[i - 1]
 
-def radixSort(nums):
-    maxDigitCount = mostDigits(nums)
-    for k in range(maxDigitCount):
-        digitBuckets = []
-        for i in range(len(nums) - 1):
-            digitBuckets[getDigit(nums[i], k)].push("hello")
-    print(range(len(nums) - 1))
+    # Place the elements in sorted order
+    i = size - 1
+    while i >= 0:
+        index = array[i] // place
+        output[count[index % 10] - 1] = array[i]
+        count[index % 10] -= 1
+        i -= 1
+
+    for i in range(0, size):
+        array[i] = output[i]
+
+
+# TC: O(n+k) || O(n+k)
+def radixSort(array):
+    # Get maximum element
+    max_element = max(array)
+
+    # Apply counting sort to sort elements based on place value.
+    place = 1
+    while max_element // place > 0:
+        countingSort(array, place)
+        place *= 10
+
 print(radixSort([23,345,5367,12,2345,9852]))     
