593a6cd5a6996ddf2ee0099af3d8ea3aca297d72
rowvln <7840167+rowvinn@users.noreply.github.com>
2022-06-12 23:06:34 -0700

    Added comments for set method for Singly Linked List

--- python/Singly_Linked_List.py
+++ python/Singly_Linked_List.py
@@ -142,6 +142,11 @@ class SinglyLinkedList:
         return current
 
     # Singly LinkedList - SET method
+    # Changing the value of a node based on its position in the Linked List
+    # This function should accept a value and an index
+    # Use your get function to find the specific node
+    # If the node is not found, return false
+    # If the node is found, set the value of that node to be the value passed to the function and return true
     def set(self, index, data):
         foundNode = self.get(index)
         if foundNode:
